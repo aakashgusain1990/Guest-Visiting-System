b2866865679e91a9bc2afa80ccc8a277de4e47e7
aakashgusain1990 <aakashgusain95@gmail.com>
2021-04-09 11:03:25 +0530

    login

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-   
-    path('' , views.home, name = 'home'),
+    path('', views.home, name = 'home'),
+    path('login/' , views.login, name = 'login'),
+    path('register/' , views.register, name = 'register'),
     path('security/' , views.security, name = 'security'),
     path('visitor/' , views.visitor, name = 'visitor'),
     
--- home/views.py
+++ home/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
-
 def home(request):
     text = "<h1>home<h1>"
     return HttpResponse(text)
-
+def login(request):
+    form = UserCreationForm()
+    context = {'form':form}
+    return render(request,'login.html',context)
+def register(request):
+    text = "<h1>register<h1>"
+    return HttpResponse(text)
 def security(request):
     text = "<h1>Security<h1>"
     return HttpResponse(text)
-
 def visitor(request):
     text = "<h1>visitor<h1>"
     return HttpResponse(text)
