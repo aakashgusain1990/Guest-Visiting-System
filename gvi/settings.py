11e28fc05853c79b1ecdbbb0a463dbc3e273b4eb
deepthi132 <deepthi2126@gmail.com>
2021-04-15 20:20:48 +0530

    Backend

--- gvi/settings.py
+++ gvi/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'hello',
         'USER': 'root',
-        'PASSWORD': 'Airtel1234',
+        'PASSWORD': '*****',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
