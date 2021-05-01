63de81d111214bb191fe87b43171132d670f16fc
deepthi132 <deepthi2126@gmail.com>
2021-05-01 22:42:03 +0530

    Front end updated

--- gvi/settings.py
+++ gvi/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+) 
+
 
 # Application definition
 
@@ -119,7 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+#STATIC_ROOT = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
--- home/forms.py
+++ home/forms.py
@@ -5,23 +5,33 @@ class facultyForm(forms.ModelForm):
     class Meta:
         model = facultyt
         fields = ["name","username","email","password"]
+        def __init__(self, *args, **kwargs):
+            super(facultyForm, self).__init__(*args, **kwargs)
+            self.fields.widget.attrs.update({'class' : 'dee'})
 class securityForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput,max_length=50)
     class Meta:
         model = securityt
         fields = ["name","username","email","password"]
+        def __init__(self, *args, **kwargs):
+            super(securityForm, self).__init__(*args, **kwargs)
+            self.fields.widget.attrs.update({'class' : 'dee'})
 class loginForm(forms.Form):
-    username = forms.CharField(label = "Username",max_length=50)
-    password = forms.CharField(label = "Password",widget=forms.PasswordInput,max_length=50) 
+    username = forms.CharField(label = "Username",max_length=50,widget=forms.TextInput(attrs={'class' : 'k'}))
+    password = forms.CharField(label = "Password",max_length=50,widget=forms.PasswordInput(attrs={'class' : 'k'})) 
 class visitorForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput,max_length=50)
     class Meta:
         model = visitort
         fields = ["name","dob","gender","username","email","password","govt_id"]
+        def __init__(self, *args, **kwargs):
+            super(visitorForm, self).__init__(*args, **kwargs)
+            self.fields.widget.attrs.update({'class' : 'dee'})
 class appointmentForm(forms.Form):
     doa = forms.DateField(label='Date of Appointment')
     timeIn = forms.TimeField(label='Time of Appointment')
     purpose = forms.CharField(widget=forms.Textarea,label='Purpose')
+    
 
 #     def clean_email(self):
 #         email = self.cleaned_data['email']
